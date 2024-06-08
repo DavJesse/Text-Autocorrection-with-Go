b5f13931d8fd8a54ea3f5b59f77c435fb47b470c
davodhiambo <jesseomolo@yahoo.com>
2024-06-09 00:01:50 +0300

    unit-testing: add TestPunct to test punct function

--- punctuations_test.go
+++ punctuations_test.go
@@ -1,1 +1,20 @@
 package main
+
+import "testing"
+
+func TestPunct(t *testing.T) {
+	testSlc := []string{"I", "was", "sitting", "over", "there", ",and", "then", "BAMM", "!!"}
+	got := cap(testSlc)
+	expected := []string{"I", "was", "sitting", "over", "there,", "and", "then", "BAMM!!"}
+
+	for i, sg := range got {
+		for j, se := range got {
+			if i == j && sg != se {
+				t.Errorf("Got: %s", got[i])
+				t.Errorf("Expected: %s", expected[j])
+				t.Errorf("TestCap Failed!")
+				t.FailNow()
+			}
+		}
+	}
+}
