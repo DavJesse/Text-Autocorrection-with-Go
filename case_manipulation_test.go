814000771949241a19433a1a50a799fcc7d1b06c
davodhiambo <jesseomolo@yahoo.com>
2024-06-08 23:20:27 +0300

    unit-testing: add TestCap to test Cap function

--- case_manipulation_test.go
+++ case_manipulation_test.go
@@ -1,2 +1,24 @@
 package main
-it
+
+import "testing"
+
+func TestCap(t *testing.T) {
+	testSlc := []string{"Welcome", "to", "the", "Brooklyn", "bridge", "(cap)"}
+	got := cap(testSlc)
+	expected := []string{"Welcome", "to", "the", "Brooklyn", "Bridge"}
+
+	for i, sg := range got {
+		for j, se := range got {
+			if i == j && sg != se {
+				t.Errorf("Got: %s", got[i])
+				t.Errorf("Expected: %s", expected[j])
+				t.Errorf("TestCap Failed!")
+				t.FailNow()
+			}
+		}
+	}
+}
+
+func TestLow(t *testing.T) {
+
+}
