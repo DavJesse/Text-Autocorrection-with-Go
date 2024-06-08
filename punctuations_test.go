00f873e99ff37e0b6c6884ae0fae90c09e56ec54
davodhiambo <jesseomolo@yahoo.com>
2024-06-09 01:18:20 +0300

    unit-testing: add TestApostrophe to test apostrophe function

--- punctuations_test.go
+++ punctuations_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPunct(t *testing.T) {
 	testSlc := []string{"I", "was", "sitting", "over", "there", ",and", "then", "BAMM", "!!"}
-	got := cap(testSlc)
+	got := punct(testSlc)
 	expected := []string{"I", "was", "sitting", "over", "there,", "and", "then", "BAMM!!"}
 
 	for i, sg := range got {
@@ -12,7 +12,24 @@ func TestPunct(t *testing.T) {
 			if i == j && sg != se {
 				t.Errorf("Got: %s", got[i])
 				t.Errorf("Expected: %s", expected[j])
-				t.Errorf("TestCap Failed!")
+				t.Errorf("TestPunct Failed!")
+				t.FailNow()
+			}
+		}
+	}
+}
+
+func TestApostrophe(t *testing.T) {
+	testSlc := []string{"I", "am", "exactly", "how", "they", "describe", "me:", "'", "awesome", "'"}
+	got := apostrophe(testSlc)
+	expected := []string{"I", "am", "exactly", "how", "they", "describe", "me:", "'awesome", "'"}
+
+	for i, sg := range got {
+		for j, se := range got {
+			if i == j && sg != se {
+				t.Errorf("Got: %s", got[i])
+				t.Errorf("Expected: %s", expected[j])
+				t.Errorf("TestApostrophe Failed!")
 				t.FailNow()
 			}
 		}
