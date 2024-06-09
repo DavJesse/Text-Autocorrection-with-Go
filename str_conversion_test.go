96ef66c63d84eacb24e6f224e41c7652d3253b64
davodhiambo <jesseomolo@yahoo.com>
2024-06-09 13:39:41 +0300

    files: add comments to str_conversion_test.go

--- str_conversion_test.go
+++ str_conversion_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestHex(t *testing.T) {
+	// Declare utility variables
 	testSlc := []string{"1E", "(hex)", "files", "were", "added"}
 	got := hex(testSlc)
 	expected := []string{"30", "files", "were", "added"}
 
+	// Compare individual elements in 'got' and 'expected' slices
 	for i, sg := range got {
 		for j, se := range got {
 			if i == j && sg != se {
@@ -20,10 +22,12 @@ func TestHex(t *testing.T) {
 }
 
 func TestBin(t *testing.T) {
+	// Declare utility variables
 	testSlc := []string{"It", "has", "been", "10", "(bin)", "years"}
 	got := bin(testSlc)
 	expected := []string{"It", "has", "been", "2", "years"}
 
+	// Compare individual elements in 'got' and 'expected' slices
 	for i, sg := range got {
 		for j, se := range got {
 			if i == j && sg != se {
