8eb4715531826af55fc6e336598ed641e387678b
davodhiambo <jesseomolo@yahoo.com>
2024-06-09 13:48:08 +0300

    files: add comments to vowel_correction_test.go

--- vowel_correction_test.go
+++ vowel_correction_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestVowels(t *testing.T) {
+	// Declare utility variables
 	testSlc := []string{"There", "it", "was.", "A", "amazing", "rock!"}
 	got := vowels(testSlc)
 	expected := []string{"There", "it", "was.", "An", "amazing", "rock!"}
 
+	// Compare individual elements in 'got' and 'expected' slices
 	for i, sg := range got {
 		for j, se := range got {
 			if i == j && sg != se {
