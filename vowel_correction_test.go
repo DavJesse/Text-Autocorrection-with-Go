95b4521aa862186d34145fd4180b5748238ea1cf
davodhiambo <jesseomolo@yahoo.com>
2024-06-09 13:07:33 +0300

    unit-testing: create TestVowels to test vowels function

--- vowel_correction_test.go
+++ vowel_correction_test.go
@@ -1,1 +1,20 @@
 package main
+
+import "testing"
+
+func TestVowels(t *testing.T) {
+	testSlc := []string{"There", "it", "was.", "A", "amazing", "rock!"}
+	got := vowels(testSlc)
+	expected := []string{"There", "it", "was.", "An", "amazing", "rock!"}
+
+	for i, sg := range got {
+		for j, se := range got {
+			if i == j && sg != se {
+				t.Errorf("Got: %s", got[i])
+				t.Errorf("Expected: %s", expected[j])
+				t.Errorf("TestVowels Failed!")
+				t.FailNow()
+			}
+		}
+	}
+}
