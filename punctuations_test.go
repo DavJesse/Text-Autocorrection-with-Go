b3fa065abfaea78f7d906c01d3fcb4bb4ca2e525
davodhiambo <jesseomolo@yahoo.com>
2024-06-09 13:36:03 +0300

    files: add comments to punctuations_test.go

--- punctuations_test.go
+++ punctuations_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestPunct(t *testing.T) {
+	// Declare utility variables
 	testSlc := []string{"I", "was", "sitting", "over", "there", ",and", "then", "BAMM", "!!"}
 	got := punct(testSlc)
 	expected := []string{"I", "was", "sitting", "over", "there,", "and", "then", "BAMM!!"}
 
+	// Compare individual elements in 'got' and 'expected' slices
 	for i, sg := range got {
 		for j, se := range got {
 			if i == j && sg != se {
@@ -20,10 +22,12 @@ func TestPunct(t *testing.T) {
 }
 
 func TestApostrophe(t *testing.T) {
+	// Declare utility variables
 	testSlc := []string{"I", "am", "exactly", "how", "they", "describe", "me:'", "awesome'"}
 	got := apostrophe(testSlc)
 	expected := []string{"I", "am", "exactly", "how", "they", "describe", "me:", "'awesome'"}
 
+	// Compare individual elements in 'got' and 'expected' slices
 	for i, sg := range got {
 		for j, se := range got {
 			if i == j && sg != se {
@@ -37,10 +41,12 @@ func TestApostrophe(t *testing.T) {
 }
 
 func TestIsQuote(t *testing.T) {
+	// Declare utility variables
 	testSlc := []byte{'\'', '"'}
 	expected := true
 	var got bool
 
+	// Compare 'got' and 'expected'
 	for _, r := range testSlc {
 		if !isQuote(r) {
 			got = isQuote(r)
