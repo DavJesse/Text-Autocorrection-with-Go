9dbdcabc4725eb60d13e77b383d28b1dcceac665
davodhiambo <jesseomolo@yahoo.com>
2024-06-09 12:43:50 +0300

    unit-testing: create TestHex to test hex function

--- str_conversion_test.go
+++ str_conversion_test.go
@@ -1,1 +1,20 @@
 package main
+
+import "testing"
+
+func TestHex(t *testing.T) {
+	testSlc := []string{"1E", "(hex)", "files", "were", "added"}
+	got := hex(testSlc)
+	expected := []string{"30", "files", "were", "added"}
+
+	for i, sg := range got {
+		for j, se := range got {
+			if i == j && sg != se {
+				t.Errorf("Got: %s", got[i])
+				t.Errorf("Expected: %s", expected[j])
+				t.Errorf("TestHex Failed!")
+				t.FailNow()
+			}
+		}
+	}
+}
